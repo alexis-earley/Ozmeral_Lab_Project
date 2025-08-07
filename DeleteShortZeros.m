5dff55ea9cdeeafe42b193cfce813423e9a44c05
alexis-earley <58522669+alexis-earley@users.noreply.github.com>
2025-08-07 10:09:02 -0400

    Update DeleteShortZeros.m
    Added comment with function description

--- DeleteShortZeros.m
+++ DeleteShortZeros.m
@@ -1,5 +1,43 @@
 function [sFiles] = DeleteShortZeros(sFiles, ProtocolPath) 
 
+% Removes closely spaced 0 triggers if they are less than 0.01 secs apart
+
+    for i = 1:length(sFiles)
+
+        CurrFile = sFiles([i]);
+
+        FName = fullfile(ProtocolPath,CurrFile.FileName);
+        z = load(FName,'F');
+        
+        FileEvents = z.F.events;
+        j = 1;
+        for j = 1:length(FileEvents)
+            FileLabel = FileEvents(j).label;
+            if FileLabel == '0'
+                break;
+            end
+        end
+
+        FileTimes = z.F.events(j).times;
+
+        NewTimes = [FileTimes(1)];
+        
+        for k = 2:length(FileTimes)
+
+            if FileTimes(k) - FileTimes(k-1) >= 0.01
+                NewTimes(end + 1) = FileTimes(k);
+            end
+        end
+
+        z.F.events(j).times = NewTimes;
+        NewEpochs = ones(1,length(NewTimes));
+        z.F.events(j).epochs = NewEpochs;
+
+        F = z;
+
+        save(FName,'F');
+
+end
     for i = 1:length(sFiles)
 
         CurrFile = sFiles([i]);
