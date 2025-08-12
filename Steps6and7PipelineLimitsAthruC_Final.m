7c929db1cdf2e655d9a3d98929ffc3fd20f471cc
alexis-earley <58522669+alexis-earley@users.noreply.github.com>
2025-08-12 10:39:27 -0400

    Update Steps6and7PipelineLimitsAthruC_Final.m

--- Steps6and7PipelineLimitsAthruC_Final.m
+++ Steps6and7PipelineLimitsAthruC_Final.m
@@ -13,14 +13,18 @@ subjTypes = {'OHI', 'OHI', 'OHI'};
 subjSettings = {'Omni', 'UltraZoom', 'Unaided'};
 %}
 
+
 subjTypes = {'ONH', 'OHI', 'OHI', 'OHI'};
 subjSettings = {'Unaided', 'Omni', 'UltraZoom', 'Unaided'};
+
 
 % All of these should be in terms of volts, not microvolts:
 upperThreshold = 100E-6; % 100E-6 by default/if commented out, put Inf to turn off
 zScoreLimit = Inf; % 6.5 by default/if commented out, put Inf to turn off
 flatStdThresh = 0; % 5E-8 by default/if commented out, put 0 to turn off
-graphBool = 0; % Mark whether to graph individual subplots in Steps 6A - 6C
+graphBool6A = 0; % Mark whether to graph individual subplots in Steps 6A - 6C
+graphBool6B = 1;
+graphBool6C = 1;
 
 maxTime = 2;
 timeRanges = [50 150; 150 350; 350 600];
@@ -37,7 +41,7 @@ step7CompOutDiffDir = fullfile(baseDir, folderName, 'Step7CompareGroupsSE_diff')
 step7CompOutDir = fullfile(baseDir, folderName, 'Step7CompareGroupsSE_filt');
 step7CompOutDiffDir = fullfile(baseDir, folderName, 'Step7CompareGroupsSE_diff_filt');
 %}
-%step7BoxOutDir = fullfile(baseDir, folderName, 'Step7CompareBoxPlots');
+step7BoxOutDir = fullfile(baseDir, folderName, 'Step7CompareBoxPlots');
 
 for i = 1:length(subjSettings)     
     subjType = subjTypes{i}; % Ex. ONH
@@ -48,6 +52,7 @@ for i = 1:length(subjSettings)
     step6AOutDir = fullfile(baseDir, folderName, 'Step6A_Output', subjType, subjSetting);
     step6BOutDir = fullfile(baseDir, folderName, 'Step6B_Output', subjType, subjSetting);
     step6COutDir = fullfile(baseDir, folderName, 'Step6C_Output', subjType, subjSetting);
+    step6C2OutDir = fullfile(baseDir, folderName, 'Step6C2_Output', subjType, subjSetting);
     %step6DPOutDir = fullfile(baseDir, folderName, 'Step6DP_Output', subjType, subjSetting);
     %step6DOutDir = fullfile(baseDir, folderName, 'Step6D_Output', subjType, subjSetting);
     step6DOutDir = fullfile(baseDir, folderName, 'Step6D_Output', subjType, subjSetting);
@@ -61,6 +66,7 @@ for i = 1:length(subjSettings)
     step7COutDir= fullfile(baseDir, folderName, 'Step7_Graph_GFPs_Output', subjType, subjSetting);
     step7DOutDir= fullfile(baseDir, folderName, 'Step7_Graph_Peaks_Output', subjType, subjSetting);
     step7EOutDir= fullfile(baseDir, folderName, 'Step7_Make_Peak_Table_Output', subjType, subjSetting);
+    step7FOutDir= fullfile(baseDir, folderName, 'Step7_Graph_GFP_Blocks_Output', subjType, subjSetting);
     step7GTitle = [subjType, ' ', subjSetting, ' - ', folderName];
      
     peakValsFolder = 'E:\Alexis_Brainstorm\EOR21_Earley_Paper_Final\Final_Testing_Structs_and_Graphs\Step6AA_Output\ONH\Unaided';
@@ -72,14 +78,18 @@ for i = 1:length(subjSettings)
     disp(['Step 6AA completed in ', num2str(toc/60), ' minutes.']);
     %}
 
-    
     %% Step 6A through 6C
     disp('Starting Step 6A thru 6C:'); tic;
     Step6OutDirs = {step6AOutDir, step6BOutDir, step6COutDir};
-    Step6AthruC(step6AInDir, Step6OutDirs, step6ACBaseDir, fullfile(subjType, subjSetting), graphBool, upperThreshold, zScoreLimit, flatStdThresh);
+    Step6AthruC(step6AInDir, Step6OutDirs, step6ACBaseDir, fullfile(subjType, subjSetting), graphBool6A, graphBool6B, graphBool6C, upperThreshold, zScoreLimit, flatStdThresh);
     %Step6AthruCPercent(step6AInDir, peakValsFolder, step6ACBaseDir, fullfile(subjType, subjSetting), [0.05, 0.95]);
     disp(['Step 6A thru 6C completed in ', num2str(toc/60), ' minutes.']);
-    
+
+%{
+     %% Step 6C2 - Combine Blocks
+    disp('Starting Step 6C2 - Combine Blocks:'); tic;
+    Step6CNew2CombinedBlocks(step6BOutDir, step6C2OutDir)
+    disp(['Step 6C2 - Combine Blocks completed in ', num2str(toc/60), ' minutes.']);
     %{
     %% Step 6D Seperate Prequel
     disp('Starting Step 6D Prequel:'); tic;
@@ -135,7 +145,7 @@ for i = 1:length(subjSettings)
 
     %% Step 7D - Graph Peaks
     disp('Starting Step 7 - Graph Peaks:'); tic;
-    Step7GraphPeaks(step6FOutDir, step7DOutDir, timeRanges);
+    Step7GraphPeaks(step6FOutDir, step7DOutDir, timeRanges, step7GTitle);
     disp(['Step 7 - Graph Peaks completed in ', num2str(toc), ' seconds.']);
 
     %% Step 7E - Make Peak Table
@@ -145,14 +155,18 @@ for i = 1:length(subjSettings)
 
     %% Step 7F - Graph GFP Blocks Overlay
     disp('Starting Step 7 - Graph GFP Blocks Overlay:'); tic;
-    Step7GraphGFP_6C2Overlay(inputDir, outputDir, maxTime)
+    Step7GraphGFP_6C2Overlay(step6C2OutDir,  step7FOutDir, maxTime)
     disp(['Step 7 - Graph GFP Blocks Overlay completed in ', num2str(toc), ' seconds.']);
 
     %% Step 7 All Subject Types - Setup
     dirList{end+1} = step6FOutDir;
     dirNames{end+1} = [subjType, ' ', subjSetting];
+
+%}
 end
 
+%{
 %% Step 7 All Subject Types
-Step7CompareAllGroupsSE_2(dirList, dirNames, [1, 4; 2, 3], step7CompOutDiffDir, maxTime)
+Step7CompareGroupsSE(dirList, dirNames, [1, 4; 2, 3], step7CompOutDiffDir, maxTime)
 Step7BoxplotCompGFP(dirList, dirNames, step7BoxOutDir);
+%}
